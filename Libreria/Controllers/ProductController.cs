189823354fb21cbd29e12cf46f8f9d9b2bc45edb
eric861129 <74950944+eric861129@users.noreply.github.com>
2021-02-04 19:58:19 +0800

    助教協助 加入 Product controller ...

---
+++ Libreria/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Libreria.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Libreria.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController()
+        {
+            _productService = new ProductService();
+        }
+
+        /// <summary>
+        /// 商品列表頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var result = _productService.GetAll();
+            return View(result);
+        }
+    }
+}
---
+++ Libreria/Controllers/SalesManController.cs
@@ -0,0 +1,23 @@
+﻿using Libreria.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Libreria.Controllers
+{
+    public class SalesManController : Controller
+    {
+        private readonly SalesManService _salesManService;
+        public SalesManController()
+        {
+            _salesManService = new SalesManService();
+        }
+        public ActionResult Index()
+        {
+            var result = _salesManService.GetAll();
+            return View(result);
+        }
+    }
+}
