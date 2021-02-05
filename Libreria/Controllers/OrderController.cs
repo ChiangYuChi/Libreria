4c41467f9277029f9cc347ed49875f2a1ec023b6
Benson <dezhengl@uci.edu>
2021-02-05 23:47:52 +0800

    加入购物车页面及其CSS档案，页面连接OrderController以及增加OrderService,新增一个_OrderLayout的布局档在Shared里面

--- Libreria/Controllers/OrderController.cs
+++ Libreria/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Libreria.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,18 @@ namespace Libreria.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly OrderService _orderService;
+
+        public OrderController()
+        {
+            _orderService = new OrderService();
+        }
+
         // GET: Order
+        /// <summary>
+        /// 购物流程主页（购物车页面）
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
