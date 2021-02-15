e85ff8821bffbd9c95ccfbe70f1065ca5f2a8668
LYC <cliying94@gmail.com>
2021-02-15 12:37:15 +0800

    merge from dev0206

---
+++ Libreria/Models/Preview.cs
@@ -0,0 +1,25 @@
+namespace Libreria.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Preview")]
+    public partial class Preview
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int PreviewId { get; set; }
+
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ImgUrl { get; set; }
+
+        public int Sort { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
