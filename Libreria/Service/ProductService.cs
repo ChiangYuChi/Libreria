7b3f841f6f1289741b4d59f94d95b3a68f5070ed
akiraa <chiangyuchi@gmail.com>
2021-02-15 18:18:43 +0800

    新增 category partialview 新增 product partialview ,category過濾功能, 排序功能

---
+++ Libreria/Service/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using Libreria.Models.EntityModel;
+using Libreria.Repository;
+using Libreria.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Libreria.Service
+{
+    public class ProductService
+    {
+
+        private readonly LibreriaRepository _DbRepository;
+
+        public ProductService()
+        {
+            _DbRepository = new LibreriaRepository();
+        }
+
+        public List<ProductViewModel> GetAll()
+        {
+            return _DbRepository.GetAll<Product>().Select(x => new ProductViewModel()
+            {
+                Id = x.ProductId,
+                Name = x.ProductName,
+                UnitPrice = x.UnitPrice,
+                Author = x.Author,
+                CreateTime = x.CreateTime,
+                CategoryId = x.CategoryId
+
+            }).ToList();
+        }
+
+
+    }
+}
