09dc1bd8bace8eef13c7374d2ebc7cf7416316a5
Kevin Lee <kevvin851222@gmail.com>
2021-02-16 11:41:51 +0800

    刪除MemberLogout

--- Libreria/Controllers/MemberCenterController.cs
+++ Libreria/Controllers/MemberCenterController.cs
@@ -14,10 +14,6 @@ namespace Libreria.Controllers
             return View();
         }
 
-        public ActionResult MemberLogout()
-        {
-            return View();
-        }
 
         public ActionResult MemberInfo()
         {
