4e38190e84155c45589e4768063c058897b419a2
Benson <dezhengl@uci.edu>
2021-02-16 13:16:10 +0800

    DataMigration

---
+++ Libreria/Migrations/202102160353507_test.cs
@@ -0,0 +1,18 @@
+namespace Libreria.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Product", "test", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Product", "test");
+        }
+    }
+}
---
+++ Libreria/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Libreria.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Libreria.Models.EntityModel.LibreriaDataModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Libreria.Models.EntityModel.LibreriaDataModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
