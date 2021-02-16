cc53048e2b55404ad6cda5a3579c0a84effb3716
akiraa <chiangyuchi@gmail.com>
2021-02-16 17:06:10 +0800

    修正sidenav 跑版

---
+++ Libreria/Service/PreviewService.cs
@@ -0,0 +1,34 @@
+﻿using Libreria.Models.EntityModel;
+using Libreria.Repository;
+using Libreria.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Libreria.Service
+{
+    public class PreviewService
+    {
+
+        private readonly LibreriaRepository _DbRepository;
+
+        public PreviewService()
+        {
+            _DbRepository = new LibreriaRepository();
+        }
+
+        public List<PreviewViewModel> GetAll()
+        {
+            return _DbRepository.GetAll<Preview>().Select(x => new PreviewViewModel()
+            {
+                ProductId = x.ProductId,
+                ImgUrl = x.ImgUrl,
+                Sort=x.Sort
+            }).ToList();
+        }
+
+
+    }
+}
---
+++ Libreria/ViewModels/PreviewViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libreria.ViewModels
+{
+    public class PreviewViewModel
+    {
+        public int ProductId { get; set; }
+        public string ImgUrl { get; set; }
+        public int Sort { get; set; }
+    }
+
+}
