466b742be5878b640ad24c63054f6c923d0b57dc
Benson <dezhengl@uci.edu>
2021-02-17 18:58:11 +0800

    资料库更新

--- Libreria/Models/EntityModel/Product.cs
+++ Libreria/Models/EntityModel/Product.cs
@@ -9,12 +9,14 @@ namespace Libreria.Models.EntityModel
     [Table("Product")]
     public partial class Product
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Product()
         {
             Favorites = new HashSet<Favorite>();
             OrderDetails = new HashSet<OrderDetail>();
             Previews = new HashSet<Preview>();
         }
+
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ProductId { get; set; }
 
@@ -53,10 +55,13 @@ namespace Libreria.Models.EntityModel
 
         public virtual Category Category { get; set; }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Favorite> Favorites { get; set; }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Preview> Previews { get; set; }
 
         public virtual Supplier Supplier { get; set; }
--- Libreria/Models/EntityModel/member.cs
+++ Libreria/Models/EntityModel/member.cs
@@ -55,8 +55,12 @@ namespace Libreria.Models.EntityModel
         [StringLength(10)]
         public string IDnumber { get; set; }
 
+        public int? RoleId { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Favorite> Favorites { get; set; }
+
+        public virtual Role Role { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order> Orders { get; set; }
