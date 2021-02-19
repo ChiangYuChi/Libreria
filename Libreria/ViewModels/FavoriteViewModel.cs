5fdba99aa68efd59d61f645109243973654163b9
Kevin Lee <kevvin851222@gmail.com>
2021-02-19 20:30:09 +0800

    加入favoriteService

---
+++ Libreria/ViewModels/FavoriteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libreria.ViewModels
+{
+    public class FavoriteViewModel
+    {
+        public int Id { get; set; }
+        public string Pic { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string SupName { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
