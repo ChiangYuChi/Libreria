e8ea3ca18470dcea9f26cb7baaf47c1326953035
LYC <cliying94@gmail.com>
2021-02-20 16:16:27 +0800

    for update

--- Libreria/Controllers/MemberCenterController.cs
+++ Libreria/Controllers/MemberCenterController.cs
@@ -19,46 +19,6 @@ namespace Libreria.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult MemberLogin(member model)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //        using (var context = new LibreriaDataModel())
-        //        {
-        //            member member = context.members
-        //                               .Where(u => u.memberId == model.memberId && u.memberPassword== model.memberPassword)
-        //                               .FirstOrDefault();
-        //            if (User != null)
-        //            {
-        //                Session["MemberName"] = member.memberName;
-        //                Session["MemberId"] = member.memberId;
-        //                return RedirectToAction("Index", "Home");
-        //            }
-        //            else
-        //            {
-        //                ModelState.AddModelError("", "帳號或密碼輸入錯誤");
-        //                return View(model);
-        //            }
-
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return View(model);
-        //    }
-
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult LogOff()
-        //{
-        //    Session["MemberName"] = string.Empty;
-        //    Session["MemberId"] = string.Empty;
-        //    return RedirectToAction("Index", "Home");
-        //}
-
-
         public ActionResult MemberInfo()
         {
             return View();
@@ -107,6 +67,10 @@ namespace Libreria.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult MemberRegisterPage(int x)
+        {
+            return View();
+        }
     }
 }
