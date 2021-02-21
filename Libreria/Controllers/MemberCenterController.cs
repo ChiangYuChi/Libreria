a1cdd943289b6737852dbf3ce8eaa4ca58fbf27d
LYC <cliying94@gmail.com>
2021-02-21 23:58:25 +0800

    加密測試中

--- Libreria/Controllers/MemberCenterController.cs
+++ Libreria/Controllers/MemberCenterController.cs
@@ -1,4 +1,5 @@
 ﻿using Libreria.Filters;
+using Libreria.Helpers;
 using Libreria.Models.EntityModel;
 using Libreria.Service;
 using Libreria.ViewModels;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Filters;
 using static Libreria.Filters.CustomAuthenticationFilter;
 
 namespace Libreria.Controllers
@@ -16,6 +18,7 @@ namespace Libreria.Controllers
     {
         private readonly FavoriteService _favoriteService;
         private readonly LibreriaDataModel _libreriaDataModel;
+        
 
 
         public MemberCenterController()
@@ -29,7 +32,6 @@ namespace Libreria.Controllers
 
         public ActionResult MemberLogin()
         {
-
             return View();
         }
 
@@ -88,7 +90,9 @@ namespace Libreria.Controllers
         {
             if (ModelState.IsValid)
             {
-                
+                //member membercheck = _libreriaDataModel.members
+             
+
                 member member = new member
                 {
                     memberName = HttpUtility.HtmlEncode(model.memberName),
@@ -97,23 +101,36 @@ namespace Libreria.Controllers
                     Address = HttpUtility.HtmlEncode(model.Address),
                     Email = HttpUtility.HtmlEncode(model.Email),
                     memberUserName = HttpUtility.HtmlEncode(model.memberUserName),
-                    memberPassword = HttpUtility.HtmlEncode(model.memberPassword),
+                    memberPassword = Utility.GetSha512(HttpUtility.HtmlEncode(model.memberPassword)),
                     birthday = DateTime.Parse(HttpUtility.HtmlEncode(model.birthday)),
                     Gender = int.Parse(HttpUtility.HtmlEncode(model.Gender)),
                     IDnumber = HttpUtility.HtmlEncode(model.IDnumber)
                 };
+                 var a = member = _libreriaDataModel.members
+                                   .Where(u => u.memberName == model.memberName)
+                                   .FirstOrDefault();
+
                 //EF
-                try
+                if (a == null)
                 {
-                    _libreriaDataModel.members.Add(member);
-                    _libreriaDataModel.SaveChanges();
-                    return Redirect("MemberLogin");
+                    try
+                    {
+                        _libreriaDataModel.members.Add(member);
+                        _libreriaDataModel.SaveChanges();
+                        return Redirect("MemberLogin");
+                    }
+                    //下列部分需要再處裡
+                    catch (Exception ex)
+                    {
+                        return Content("新增帳號失敗:" + ex.ToString());
+                    }
                 }
-                //下列部分需要再處裡
-                catch(Exception ex)
+                else
                 {
-                    return Content("新增帳號失敗:" + ex.ToString());
+                    ModelState.AddModelError("", "*帳號已被使用");
+                    return View(model);
                 }
+                
             }
             return View();
         }
@@ -129,7 +146,7 @@ namespace Libreria.Controllers
         [HttpPost]
         public string AddToFavorite(ProductViewModel productVM)
         {
-            var CanTakeMemberNameFromThisVariable = System.Web.HttpContext.Current.Session["MemberID"];
+            var CanTakeMemberNameFromThisVariable = System.Web.HttpContext.Current.Session["MemberName"];
             var result = _favoriteService.Create(productVM);
 
             if (result.IsSuccessful)
