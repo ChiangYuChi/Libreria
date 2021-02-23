b54df86f8ef8f0ffeb79f0ca3efd65f59acae5e3
Benson <dezhengl@uci.edu>
2021-02-23 15:52:16 +0800

    更新资料库

--- Libreria/Migrations/Configuration.cs
+++ Libreria/Migrations/Configuration.cs
@@ -1,7 +1,5 @@
 namespace Libreria.Migrations
 {
-    using Libreria.Helpers;
-    using Libreria.Models.EntityModel;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -20,27 +18,6 @@ namespace Libreria.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-
-            context.members.AddOrUpdate(x => x.memberId,
-                new member() { memberId = 1, memberName = "Libreria01", MobileNumber = "0900000001", HomeNumber = "(00)00000000", Address = "1", Email = "Libreria01@gmail.com", memberUserName = "Libreria01", memberPassword = "@Libreria01",IDnumber="A123456789" , RoleId = 1 });
-            context.Roles.AddOrUpdate(x => x.RoleID,
-                new Role() { RoleID = 1, RoleName = "Libreria01"});
-            context.members.AddOrUpdate(x => x.memberId,
-               new member() { memberId = 2, memberName = "Libreria02", MobileNumber = "0900000002", HomeNumber = "(00)00000002", Address = "2", Email = "Libreria02@gmail.com", memberUserName = "Libreria02", memberPassword = "@Libreria02", IDnumber = "B123456789", RoleId = 2 });
-            context.Roles.AddOrUpdate(x => x.RoleID,
-                new Role() { RoleID = 2, RoleName = "Libreria02" });
-            context.members.AddOrUpdate(x => x.memberId,
-               new member() { memberId = 3, memberName = "Libreria03", MobileNumber = "0900000003", HomeNumber = "(00)00000003", Address = "3", Email = "Libreria03@gmail.com", memberUserName = "Libreria03", memberPassword = Utility.GetSha512("@Libreria03"), IDnumber = "C123456789", RoleId = 3 });
-            context.Roles.AddOrUpdate(x => x.RoleID,
-                new Role() { RoleID = 3, RoleName = "Libreria03" });
-            context.members.AddOrUpdate(x => x.memberId,
-               new member() { memberId = 4, memberName = "Libreria04", MobileNumber = "0900000004", HomeNumber = "(00)00000000", Address = "4", Email = "Libreria04@gmail.com", memberUserName = "Libreria04", memberPassword = Utility.GetSha512("@Libreria04"), IDnumber = "D123456789", RoleId = 4 });
-            context.Roles.AddOrUpdate(x => x.RoleID,
-                new Role() { RoleID = 4, RoleName = "Libreria04" });
-            context.members.AddOrUpdate(x => x.memberId,
-               new member() { memberId = 5, memberName = "Libreria05", MobileNumber = "0900000005", HomeNumber = "(00)00000000", Address = "5", Email = "Libreria05@gmail.com", memberUserName = "Libreria05", memberPassword = Utility.GetSha512("@Libreria05"), IDnumber = "E123456789", RoleId = 5 });
-            context.Roles.AddOrUpdate(x => x.RoleID,
-                new Role() { RoleID = 5, RoleName = "Libreria05" });
         }
     }
 }
