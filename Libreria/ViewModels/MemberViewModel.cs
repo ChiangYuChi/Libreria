ebab79a633dc4fcc71b4fc73f240fd9d81f07310
eric861129 <74950944+eric861129@users.noreply.github.com>
2021-02-24 13:34:43 +0800

    註冊頁面調整完畢

--- Libreria/ViewModels/MemberViewModel.cs
+++ Libreria/ViewModels/MemberViewModel.cs
@@ -14,16 +14,20 @@ namespace Libreria.ViewModels
         /// </summary>
         [Required]
         [StringLength(50)]
+        [Display(Name = "帳號")]
         public string memberName { get; set; }
         /// <summary>
         /// 手機號碼
         /// </summary>
+        [Display(Name = "手機號碼")]
         [Required]
         [StringLength(50)]
         public string MobileNumber { get; set; }
         /// <summary>
         /// 市話
         /// </summary>
+        [Display(Name = "市話")]
+
         [StringLength(50)]
         public string HomeNumber { get; set; }
         /// <summary>
@@ -31,12 +35,16 @@ namespace Libreria.ViewModels
         /// </summary>
         [Required]
         [StringLength(100)]
+        [Display(Name = "地址")]
+
         public string Address { get; set; }
         /// <summary>
         /// 電子郵件
         /// </summary>
         [Required]
         [StringLength(50)]
+        [Display(Name = "電子郵件")]
+
         public string Email { get; set; }
 
         /// <summary>
@@ -44,33 +52,45 @@ namespace Libreria.ViewModels
         /// </summary>
         [Required]
         [StringLength(50)]
+        [Display(Name = "姓名")]
+
         public string memberUserName { get; set; }
         /// <summary>
         /// 密碼
         /// </summary>
         [Required]
         [StringLength(50)]
+        [Display(Name = "密碼")]
+
         public string memberPassword { get; set; }
         /// <summary>
         /// 確認密碼專用欄位，不影響資料庫
         /// </summary>
         [NotMapped]
         [Compare("memberPassword")]
+        [Display(Name = "確認密碼")]
+
         public string confirmPassword { get; set; }
 
         /// <summary>
         /// 生日
         /// </summary>
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "生日")]
+
         public DateTime birthday { get; set; }
         /// <summary>
-        /// 姓名
+        /// 性別
         /// </summary>
+        [Display(Name = "性別")]
+        /// 
         public int Gender { get; set; }
         /// <summary>
         /// 身分證字號
         /// </summary>
+        [Display(Name = "身分證字號")]
+
         [Required]
         [StringLength(10)]
         public string IDnumber { get; set; }
