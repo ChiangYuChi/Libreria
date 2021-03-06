7c745233e18cdaffd78b70fd79013efbbc01bb39
akiraa <chiangyuchi@gmail.com>
2021-03-06 15:52:59 +0800

    新增 PaymentState 欄位

---
+++ Libreria/Migrations/202103060750436_PaymentState.cs
@@ -0,0 +1,18 @@
+namespace Libreria.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PaymentState : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Orders", "PaymentState", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Orders", "PaymentState");
+        }
+    }
+}
