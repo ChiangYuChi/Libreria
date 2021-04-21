7af73b10fbe633cb3c7d3de74a595db5bd930f30
joyce <joyce781108@gmail.com>
2021-04-21 18:37:23 +0800

    修改

--- Libreria/Service/RentalService.cs
+++ Libreria/Service/RentalService.cs
@@ -131,9 +131,9 @@ namespace Libreria.Service
                     oPayment.MerchantID = "2000132";//ECPay提供的特店編號
 
                     /* 基本參數 */
-                    oPayment.Send.ReturnURL = "https://localhost:44330/Rental/ConfirmBooling";//付款完成通知回傳的網址
+                    oPayment.Send.ReturnURL = "https://weblibreria.azurewebsites.net//Rental/ConfirmBooling";//付款完成通知回傳的網址
                     oPayment.Send.ClientBackURL = "http://127.0.0.1:4040";//瀏覽器端返回的廠商網址
-                    oPayment.Send.OrderResultURL = $"https://localhost:44330/Rental/PayReturnDetail?orderId={rentalVM.ExOrderId}";//瀏覽器端回傳付款結果網址
+                    oPayment.Send.OrderResultURL = $"https://weblibreria.azurewebsites.net/Rental/PayReturnDetail?orderId={rentalVM.ExOrderId}";//瀏覽器端回傳付款結果網址
                     oPayment.Send.MerchantTradeNo = "n" + "ECPay" + new Random().Next(0, 99999).ToString();//廠商的交易編號
                     oPayment.Send.MerchantTradeDate = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");//廠商的交易時間
                     /*oPayment.Send.TotalAmount = Decimal.Parse($"{ orderVM.OrderPrice}") */
